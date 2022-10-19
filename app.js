034c95234146dc70132b5c7d82bb20007607abc9
Gustavo H. Fazam <gufazam10@gmail.com>
2022-10-18 21:06:10 -0300

    programa pra calculo de média

---
+++ app.js
@@ -0,0 +1,13 @@
+console.log("Olá, estou funcionando")
+console.log(10+3-5)
+
+let nome = "Gustavo"
+let nota1 = 6.74
+let nota2 = 5.6
+let nota3 = 7.3
+let nota4 = 4.44
+let media = (nota1 + nota2 + nota3 + nota4) / 4
+let mediaFixada = media.toFixed(1)
+
+console.log("Olá" + nome)
+console.log("Sua media foi: " + mediaFixada)
